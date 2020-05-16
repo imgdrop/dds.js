88fd345c55627af88e6c8e5c8673c2a81384463a
matanui159 <josh@minteronline.com>
2020-05-16 11:36:41 +1000

    First NPM deploy

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 [![CI](https://github.com/imgdrop/dds.js/workflows/CI/badge.svg?branch=master)](https://github.com/imgdrop/dds.js/actions)
 [![NPM size](https://img.shields.io/bundlephobia/min/@imgdrop/dds)](https://npmjs.com/package/@imgdrop/dds)
 
-The DDS decoder used by [Image Drop](https://imgdrop.app). Supports DXT1 to DXT5. DX10 mode is not currently supported.
+The DDS decoder used by [Image Drop](https://imgdrop.app). Supports DXT1 to DXT5. DX10 mode and DXT1 alpha is not currently supported.
--- src/index.ts
+++ src/index.ts
@@ -43,6 +43,58 @@ function decodeBC1(chunk: Uint8Array, block: Uint8Array): void {
    setValue(15, 7, 6);
 }
 
+function decodeBC4(chunk: Uint8Array, block: Uint8Array): void {
+   const alpha0 = chunk[0];
+   const alpha1 = chunk[1];
+   const alpha = [
+      alpha0,
+      alpha1,
+      (6 * alpha0 + alpha1) / 7,
+      (5 * alpha0 + 2 * alpha1) / 7,
+      (4 * alpha0 + 3 * alpha1) / 7,
+      (3 * alpha0 + 4 * alpha1) / 7,
+      (2 * alpha0 + 5 * alpha1) / 7,
+      (alpha0 + 6 * alpha1) / 7
+   ];
+
+   const setValue = (index: number, byte: number, shift: number): void => {
+      let bits = (chunk[byte] >>> shift) & 0x7;
+      if (shift === 6) {
+         bits |= (chunk[byte + 1] & 0x1) << 2;
+      } else if (shift === 7) {
+         bits |= (chunk[byte + 1] & 0x3) << 1; 
+      }
+      block[index * 4 + 3] = alpha[bits];
+   }
+   setValue(0, 2, 0);
+   setValue(1, 2, 3);
+   setValue(2, 2, 6);
+   setValue(3, 3, 1);
+   setValue(4, 3, 4);
+   setValue(5, 3, 7);
+   setValue(6, 4, 2);
+   setValue(7, 4, 5);
+   setValue(8, 5, 0);
+   setValue(9, 5, 3);
+   setValue(10, 5, 6);
+   setValue(11, 6, 1);
+   setValue(12, 6, 4);
+   setValue(13, 6, 7);
+   setValue(14, 7, 2);
+   setValue(15, 7, 5);
+}
+
+function unmultiplyAlpha(block: Uint8Array): void {
+   for (let i = 0; i < 64; i += 4) {
+      const alpha = block[i + 3] / 0xFF;
+      if (alpha !== 0) {
+         block[i + 0] = Math.min(block[i + 0] / alpha, 0xFF);
+         block[i + 1] = Math.min(block[i + 1] / alpha, 0xFF);
+         block[i + 2] = Math.min(block[i + 2] / alpha, 0xFF);
+      }
+   }
+}
+
 export class DDSDecoder {
    private buffer?: Uint8Array;
 
@@ -115,6 +167,34 @@ export class DDSDecoder {
       });
    }
 
+   private decodeDXT3(unmultiply: boolean): void {
+      const blockSize = 16;
+      const chunks = this.readUint8(this.blockCount * blockSize);
+      this.decodeChunks((index, block) => {
+         for (let i = 0; i < 8; i += 1) {
+            const alpha = chunks[index * blockSize + i];
+            block[i * 8 + 3] = (alpha & 0xF) * 17;
+            block[i * 8 + 7] = (alpha >>> 4) * 17;
+         }
+         decodeBC1(chunks.subarray(index * blockSize + 8), block);
+         if (unmultiply) {
+            unmultiplyAlpha(block);
+         }
+      });
+   }
+
+   private decodeDXT5(unmultiply: boolean): void {
+      const blockSize = 16;
+      const chunks = this.readUint8(this.blockCount * blockSize);
+      this.decodeChunks((index, block) => {
+         decodeBC4(chunks.subarray(index * blockSize), block);
+         decodeBC1(chunks.subarray(index * blockSize + 8), block);
+         if (unmultiply) {
+            unmultiplyAlpha(block);
+         }
+      });
+   }
+
    decode(): void {
       const magic = this.readInt32(1);
       if (magic[0] !== 0x20534444) {
@@ -143,9 +223,17 @@ export class DDSDecoder {
             this.decodeDXT1();
             return;
          case 0x32545844: // DXT2
+            this.decodeDXT3(true);
+            break;
          case 0x33545844: // DXT3
+            this.decodeDXT3(false);
+            break;
          case 0x34545844: // DXT4
+            this.decodeDXT5(true);
+            break;
          case 0x35545844: // DXT5
+            this.decodeDXT5(false);
+            break;
          default:
             throw new Error(`Unknown FourCC: ${getFourCCName(fourcc)}`);
       }
